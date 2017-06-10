d66ee1406ac63a79b219e8b1698940365a5fcf32
Jerome Leclanche <jerome@leclan.ch>
2017-06-10 19:01:05 +0300

    Clone more repositories by default

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,12 +6,37 @@ GITHUB="git@github.com:HearthSim"
 
 HSREPLAYNET_GIT="$GITHUB/HSReplay.net"
 JOUST_GIT="$GITHUB/Joust"
+HEARTHSIM_IDENTITY_GIT="$GITHUB/hearthsim-identity"
+PYTHON_HEARTHSTONE_GIT="$GITHUB/python-hearthstone"
+PYTHON_HSLOG_GIT="$GITHUB/python-hslog"
+HSREPLAY_GIT="$GITHUB/hsreplay"
+HSREDSHIFT_GIT="$GITHUB/hsredshift"
 
 
 cd "$BASEDIR" || exit 1
 
 if [[ ! -d $BASEDIR/HSReplay.net ]]; then
 	git clone "$HSREPLAYNET_GIT"
+fi
+
+if [[ ! -d $BASEDIR/hearthsim-identity ]]; then
+	git clone "$HEARTHSIM_IDENTITY_GIT"
+fi
+
+if [[ ! -d $BASEDIR/python-hearthstone ]]; then
+	git clone "$PYTHON_HEARTHSTONE_GIT"
+fi
+
+if [[ ! -d $BASEDIR/python-hslog ]]; then
+	git clone "$PYTHON_HSLOG_GIT"
+fi
+
+if [[ ! -d $BASEDIR/hsredshift ]]; then
+	git clone "$HSREDSHIFT_GIT"
+fi
+
+if [[ ! -d $BASEDIR/hsreplay ]]; then
+	git clone "$HSREPLAY_GIT"
 fi
 
 if [[ ! -d $BASEDIR/Joust ]]; then
