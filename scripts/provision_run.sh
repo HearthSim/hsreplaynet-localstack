d0896d933bb217f85972368d4154de9d775a7e15
Jerome Leclanche <jerome@leclan.ch>
2017-11-26 21:53:47 +0200

    Drop rqworker

--- scripts/provision_run.sh
+++ scripts/provision_run.sh
@@ -15,15 +15,12 @@ echo "Starting Django server"
 python "$HSREPLAYNET/manage.py" runserver 0.0.0.0:8000 &
 
 echo "Starting Webpack watcher"
-PYTHONPATH="$HSREPLAYNET" $NODE_MODULES/webpack/bin/webpack.js -d \
+PYTHONPATH="$HSREPLAYNET" "$NODE_MODULES/webpack/bin/webpack.js" -d \
 	--devtool cheap-module-eval-source-map \
 	--env.cache \
 	--config "$HSREPLAYNET/webpack.config.js" \
 	--progress \
 	--watch &
-
-echo "Starting RQ Workers"
-python "$HSREPLAYNET/manage.py" rqworker &
 
 echo "Starting Django SSL server"
 python "$HSREPLAYNET/manage.py" runsslserver 0.0.0.0:8443
