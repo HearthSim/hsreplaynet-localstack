f34c07ec4d63e72990bc84ef99752678504d76e8
Jerome Leclanche <jerome@leclan.ch>
2018-03-23 21:49:09 +0200

    Run load_cards at init time

--- initdb.sh
+++ initdb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker-compose run django pipenv run django-admin.py migrate
 docker-compose run django pipenv run python /opt/hsreplay.net/initdb.py
+docker-compose run django pipenv run django-admin.py load_cards
