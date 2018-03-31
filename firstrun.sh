bfe2f8e7394380e60b6493eee7d049e6a2fa0a18
Jerome Leclanche <jerome@leclan.ch>
2018-03-31 17:42:30 -0400

    Run pipenv install on firstrun only

--- firstrun.sh
+++ firstrun.sh
@@ -16,6 +16,7 @@ docker-compose build
 docker-compose pull
 
 docker-compose run django /opt/hsreplay.net/source/scripts/get_vendor_static.sh
+docker-compose run django pipenv install --dev --skip-lock
 
 echo
 echo "------------------------------------------------"
