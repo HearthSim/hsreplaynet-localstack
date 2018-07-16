a988d795cae5e99e76cef24faf2a976e1107d9af
Jerome Leclanche <jerome@leclan.ch>
2018-07-16 16:06:21 +0300

    Use exec instead of run in initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose run django pipenv run django-admin.py migrate
-docker-compose run django pipenv run python /opt/hsreplay.net/initdb.py
-docker-compose run django pipenv run django-admin.py load_cards
+docker-compose exec django pipenv run django-admin.py migrate
+docker-compose exec django pipenv run python /opt/hsreplay.net/initdb.py
+docker-compose exec django pipenv run django-admin.py load_cards
