b49c62864c7cac41ded6ddffe4c3f5e450d5f289
Jerome Leclanche <jerome@leclan.ch>
2018-07-16 17:08:18 +0300

    Use python3.6 to build hsredshift_udfs and install on 2.7+3.6

--- redshift/10_hsredshift.sh
+++ redshift/10_hsredshift.sh
@@ -2,6 +2,6 @@ cd /tmp/hsredshift/udfs
 
 export DB_CONNECTION=postgresql://postgres@:5432/dev
 
-/usr/bin/python2.7 ./setup.py load_into_postgres
-/usr/bin/python3.5 -m hsredshift.etl.models --apply
-/usr/bin/python3.5 -m hsredshift.etl.views --apply
+/usr/bin/python3.6 ./setup.py load_into_postgres
+/usr/bin/python3.6 -m hsredshift.etl.models --apply
+/usr/bin/python3.6 -m hsredshift.etl.views --apply
