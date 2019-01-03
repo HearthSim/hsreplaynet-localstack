89d83b7f66d75f0f26f9ccba65b357173709117c
Benedict Etzel <benedict@hearthsim.net>
2019-01-03 14:48:13 +0100

    Fix Redshift python version

--- redshift/10_hsredshift.sh
+++ redshift/10_hsredshift.sh
@@ -2,6 +2,6 @@ cd /tmp/hsredshift/udfs
 
 export DB_CONNECTION=postgresql://postgres@:5432/dev
 
-/usr/bin/python3.6 ./setup.py load_into_postgres
-/usr/bin/python3.6 -m hsredshift.etl.models --apply
-/usr/bin/python3.6 -m hsredshift.etl.views --apply
+/usr/bin/python3.7 ./setup.py load_into_postgres
+/usr/bin/python3.7 -m hsredshift.etl.models --apply
+/usr/bin/python3.7 -m hsredshift.etl.views --apply
