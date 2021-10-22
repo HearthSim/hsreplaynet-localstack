a1ecaf9721ffd3d6b0d610abbe5cff1864252ee3
Édipo Souza <edipo2s@gmail.com>
2021-10-22 14:00:56 -0300

    Load mercenary cards on initdb script

--- initdb.sh
+++ initdb.sh
@@ -2,3 +2,4 @@
 docker-compose exec django pipenv run django-admin.py migrate
 docker-compose exec django pipenv run python /opt/hsreplay.net/initdb.py
 docker-compose exec django pipenv run django-admin.py load_cards
+docker-compose exec django pipenv run django-admin.py load_mercenaries_cards
