a2c64edc07927fb0be5cc2081b439e57e7482902
Benedict Etzel <benedict@hearthsim.net>
2023-03-22 11:24:21 +0100

    Replace deprecated django-admin.py call

--- initdb.sh
+++ initdb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose exec django pipenv run django-admin.py migrate
+docker-compose exec django pipenv run django-admin migrate
 docker-compose exec django pipenv run python /opt/hsreplay.net/initdb.py
-docker-compose exec django pipenv run django-admin.py load_cards
-docker-compose exec django pipenv run django-admin.py load_mercenaries
+docker-compose exec django pipenv run django-admin load_cards
+docker-compose exec django pipenv run django-admin load_mercenaries
