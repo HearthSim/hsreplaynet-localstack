993be27ed51d9f9dc7c46f09fd9b24585978b627
Benedict Etzel <benedict@hearthsim.net>
2023-08-28 16:40:01 +0200

    chore: update win_firstrun.ps1 to match linux one

--- win_firstrun.ps1
+++ win_firstrun.ps1
@@ -1,7 +1,11 @@
 $basedir = "."
 
 if (-not (Test-Path "$basedir/HSReplay.net")) {
-	git clone "https://github.com/hearthsim/HSReplay.net" "$basedir/HSReplay.net"
+	git clone "git@github.com:HearthSim/HSReplay.net" "$basedir/HSReplay.net"
+}
+
+if (-not (Test-Path "$basedir/HSReplay.net/locale")) {
+	git clone "git@github.com:HearthSim/hsreplaynet-i18n" "$basedir/HSReplay.net/locale"
 }
 
 $localSettings = "$basedir/HSReplay.net/hsreplaynet/local_settings.py"
@@ -9,6 +13,15 @@ if (-not (Test-Path $localSettings)) {
 	"Generating new local_settings.py"
 	Copy-Item "$basedir/scripts/local_settings.py" $localSettings
 }
+
+if (-not (Test-Path "$basedir/hsreplaynet-dev-proxy")) {
+	git clone "git@github.com:HearthSim/hsreplaynet-dev-proxy.git" "$basedir/hsreplaynet-dev-proxy"
+}
+
+if (-not (Test-Path "$basedir/.env")) {
+    "HSREPLAYNET_SESSIONID=your_sessionid_here" >> "$basedir/.env"
+}
+
 
 "Running build"
 Invoke-Expression "docker-compose build"
